d41986acb67c33312f1a3994cfe89fc4cf7b6048
Mahitha <guda.mahitha@capgemini.com>
2025-06-11 17:05:25 +0530

    filter elements from table and delete

--- FilterTable/script.js
+++ FilterTable/script.js
@@ -1,10 +1,30 @@
+//search filter value from input
 document.getElementById('searchInput').addEventListener('keyup', function () {
     const filter = this.value.toLowerCase();
     const rows = document.querySelectorAll('#tableBody tr');
+    let matchFound = false;
   
     rows.forEach(row => {
       const text = row.textContent.toLowerCase();
-      row.style.display = text.includes(filter) ? '' : 'none';
+      const isMatch = text.includes(filter);
+      row.style.display = isMatch ? '' : 'none';
+      if (isMatch) matchFound = true;
     });
+  //if element doesn't exist in table then error msg
+    const errorMsg = document.getElementById('errorMsg');
+    if (!matchFound && filter !== '') {
+      errorMsg.style.display = 'block';
+    } else {
+      errorMsg.style.display = 'none';
+    }
   });
-   
+  
+  /// delete logic
+document.querySelectorAll('.delete-btn').forEach(button => {
+      button.addEventListener('click', function () {
+        const row = this.closest('tr');
+        row.remove();
+      });
+    });
+    
+  
